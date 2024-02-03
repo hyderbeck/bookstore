ebd40f8cb38fcdedc48682aaade38d23190dfd46
Hyder Beck <hyderbeck9@gmail.com>
2024-02-03 22:56:46 +0300

    feat: draft homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,69 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Bookstore</h1>
-    </main>
+    <>
+      <header>
+        <h1>
+          <Link href="/">Bookstore</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link href="">Browse</Link>
+            </li>
+          </ul>
+        </nav>
+        <form>
+          <input
+            type="search"
+            placeholder="Search for a book..."
+            aria-label="Search"
+          />
+          <button>Search</button>
+        </form>
+        <button>Cart</button>
+      </header>
+      <main>
+        {books.map((book) => (
+          <article key={book.id}>
+            <h2>{book.title}</h2>
+            <p>{book.author}</p>
+            <Image
+              src="https://placehold.co/200/white/white.png"
+              width={200}
+              height={200}
+              alt={book.title}
+            />
+            <p>${book.price}</p>
+          </article>
+        ))}
+      </main>
+      <footer>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+        </ul>
+        <p>&copy; 2024</p>
+      </footer>
+    </>
   );
 }
+
+const books = [
+  { id: 1, title: "To Kill a Mockingbird", author: "Harper Lee", price: 9.99 },
+  {
+    id: 2,
+    title: "Crime and Punishment",
+    author: "Fyodor Dostoevsky",
+    price: 12.99,
+  },
+  {
+    id: 3,
+    title: "The Old Man and the Sea",
+    author: "Ernest Hemingway",
+    price: 8.99,
+  },
+];
